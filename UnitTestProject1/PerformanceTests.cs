de85c313967079e8cfc04adfdc9db00cb7cf173c
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-05-21 21:21:00 +0100

    Completed Performance test on searching for triangle given a point

--- UnitTestProject1/PerformanceTests.cs
+++ UnitTestProject1/PerformanceTests.cs
@@ -12,61 +12,68 @@ namespace UnitTestProject1
     {
         public PerformanceTests()
         {
-            CountOfPoints = 100;
         }
-        public int CountOfPoints { get; set; }
         /// <summary>
         /// In this test we are picking a Point at random and finding all Triangles which contain that Point
-        /// In this test we are verifying that when querying for a Triangle with a given point, 
-        /// Sqllite foreign keys are being used to speed up querying
+        /// We are doing this with 1)Simple List and 2)Inner joins on SQLlite
         /// </summary>
         [TestMethod]
-        public void Query_For_TriangleGiven_Point_Using_SqlLite_WORK_IN_PROGRESS()
+        public void Query_For_TriangleGive_Point()
         {
             Random rnd = new Random(DateTime.Now.Second);
-            var pts =utils.Util.CreateRandomPoints(0, 100, CountOfPoints);
-            var triangles=utils.Util.FindAllTriangles(pts);
-            DemoLib.SqlDbContext ctx = utils.Util.CreateSqlLiteContext();
-            ctx.Points.AddRange(pts);
-            ctx.Triangles.AddRange(triangles);
-            ctx.SaveChanges();
-            int[] idsOfPointsRandom = pts.Select(p => p.ID).OrderBy(id => rnd.Next()).ToArray();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            var q = ctx.Triangles.AsQueryable();
-            foreach (int idOfPoint in idsOfPointsRandom)
+            long timetakenUsingBasicLists = 0;
+            int countOfPoints = 100;
+            ///
+            /// Using simple List
+            ///
             {
-                var tri = from t in ctx.Triangles join tv in ctx.TriangleVertices on t.ID equals tv.ParentID where tv.PointID == idOfPoint select t;
+                Trace.WriteLine("Simple lists");
+                Trace.WriteLine("----------------------------");
+                var lstPoints = utils.Util.CreateRandomPoints(0, 100, countOfPoints).ToList();
+                var lstTriangles = utils.Util.FindAllTriangles(lstPoints).ToList();
+                int[] idsOfPointsRandom = lstPoints.Select(p => p.ID).OrderBy(id => rnd.Next()).ToArray();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                foreach (int idOfPoint in idsOfPointsRandom)
+                {
+                    var tri = lstTriangles.Where(t => t.Vertices.Any(tv => tv.Vertex.ID == idOfPoint)).ToArray();
+                    Assert.IsNotNull(tri);
+                    Assert.IsTrue(tri.Length > 0);
+                }
+                sw.Stop();
+                timetakenUsingBasicLists = sw.ElapsedMilliseconds;
+                Trace.WriteLine($"List<>-Time taken to query for {idsOfPointsRandom.Length} points and '{lstTriangles.Count()}' triangles was {timetakenUsingBasicLists} ms ");
+            }
+            long timetakenUsingSqlLite = 0;
+            ///
+            /// Using SQLite
+            ///
+            {
+                Trace.WriteLine("SQLlite");
+                Trace.WriteLine("----------------------------");
+                var pts = utils.Util.CreateRandomPoints(0, 100, countOfPoints);
+                var triangles = utils.Util.FindAllTriangles(pts);
+                DemoLib.SqlDbContext ctx = utils.Util.CreateSqlLiteContext();
+                ctx.Points.AddRange(pts);
+                ctx.Triangles.AddRange(triangles);
+                ctx.SaveChanges();
+                int[] idsOfPointsRandom = pts.Select(p => p.ID).OrderBy(id => rnd.Next()).ToArray();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                var q = ctx.Triangles.AsQueryable();
+                foreach (int idOfPoint in idsOfPointsRandom)
+                {
+                    var tri = from t in ctx.Triangles join tv in ctx.TriangleVertices on t.ID equals tv.ParentID where tv.PointID == idOfPoint select t;
                     //ctx.Triangles.Single(t=>t.ve)
                     //AsQueryable().in.Single(t => t.Vertices.Any(tv => tv.Vertex.ID == idOfPoint)).ToArray();
-                Assert.IsTrue(tri.Count() > 0);
-                //Assert.IsTrue(tri.Vertices.Any(tv => tv.PointID == idOfPoint));
+                    Assert.IsTrue(tri.Count() > 0);
+                    //Assert.IsTrue(tri.Vertices.Any(tv => tv.PointID == idOfPoint));
+                }
+                sw.Stop();
+                timetakenUsingSqlLite = sw.ElapsedMilliseconds;
+                Trace.WriteLine($"SQLLite-Time taken to query for '{idsOfPointsRandom.Length}' points and '{triangles.Length}' triangles was {timetakenUsingSqlLite} ms ");
             }
-            sw.Stop();
-            Trace.WriteLine($"Time taken to query for '{idsOfPointsRandom.Length}' points and '{triangles.Length}' triangles was {sw.ElapsedMilliseconds} ms ");
-        }
-        /// <summary>
-        /// In this test we are picking a Point at random and finding all Triangles which contain that Point
-        /// </summary>
-        [TestMethod]
-        public void Query_For_TriangleGive_Point_Basic_Lists_WORK_IN_PROGRESS()
-        {
-            Random rnd = new Random(DateTime.Now.Second);
-            var lstPoints = utils.Util.CreateRandomPoints(0, 100, CountOfPoints).ToList();
-            var lstTriangles = utils.Util.FindAllTriangles(lstPoints).ToList();
-            int[] idsOfPointsRandom = lstPoints.Select(p => p.ID).OrderBy(id => rnd.Next()).ToArray();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            foreach (int idOfPoint in idsOfPointsRandom)
-            {
-                var tri = lstTriangles.Where(t => t.Vertices.Any(tv => tv.Vertex.ID == idOfPoint)).ToArray();
-                Assert.IsNotNull(tri);
-                Assert.IsTrue(tri.Length > 0);
-                //Assert.IsTrue(tri.Vertices.Any(tv => tv.Vertex.ID == idOfPoint));
-            }
-            sw.Stop();
-            Trace.WriteLine($"Time taken to query for {idsOfPointsRandom.Length} points and '{lstTriangles.Count()}' triangles was {sw.ElapsedMilliseconds} ms ");
-
+            Assert.IsTrue(timetakenUsingSqlLite < 0.5 * timetakenUsingBasicLists);
         }
         /// <summary>
         /// We are comparing the performance of Find() method vs doing a full scan through List<>
